472a2408afe66cf04b970b3411ed0d59185a3542
lamis15 <lamis.mhimdi@esprit.tn>
2026-02-05 23:38:46 +0100

    Initial commit - Workshop Angular n°2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';  // Import obligatoire pour ngModel
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListSuggestionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule  // ⚠️ IMPORTANT: Nécessaire pour utiliser [(ngModel)] dans les templates
   ],
   providers: [],
   bootstrap: [AppComponent]
